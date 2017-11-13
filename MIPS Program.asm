1b57292134db3c101a58815d0e6d065ccc3f82ec
cicelybeckford <cicelybeckford@users.noreply.github.com>
2017-11-13 02:36:25 -0500

    Fixed errors

--- MIPS Program.asm
+++ MIPS Program.asm
@@ -25,39 +25,46 @@ main:
 				 li $a1, 32                	  #allot the byte space for hexadecimal
 				 syscall
 	
+         addi $a1, $a0, 0        		  #move address of hexadecimal into $a1
 				 jal STRLEN                   #call strlen procedure
-				 addi $a1, $a0, 0        			#move address of hexadecimal into $a1
   			 addi $s3, $t2, -1            #store length of string in s3 register
-				 addi $t3, $zero, 8      			#initialize $t3 to 8
-				 ble $s3, $t3, IF         		#if string length less than or equal to 8 branch to IF
-				 bgt $s3, $t3, ELSE       		#if string length greater than 8 branch to ELSE
+				 addi $s0, $zero, 8      			#initialize $t3 to 8
+				 ble $s3, $s0, IF         		#if string length less than or equal to 8 branch to IF
+				 bgt $s3, $s0, ELSE       		#if string length greater than 8 branch to ELSE
         
   IF:   
-         li $s1, 0						       #initialize decimal value to 0
+         li $s1, 0						        #initialize decimal value to 0
   			 li $t2, 1										#loop counter
   			 li $s4 '0'              		  #holds character '0'
   			 li $s5, '9' 									#holds character '9'
   			 li $s6, 'a' 									#holds character 'a'
   			 li $s7, 'f'							    #holds character 'f'
   			 li $t4, 'A'						    	#holds character 'A'
-  			 li $t5, 'F'						    	#holds character 'F'
-  			 
+  			 li $t5, 'F'						    	#holds character 'F'			 
+  
   FORLOOP:
-            beq $t2, $s3, PRINT       #if counter equals string length end loop
+            bgt $t2, $s3, PRINT       #if counter greater than string length end loop
             lb $t3, ($a1)             #load the next character into t3
             bge $t3, $s4, AND         #if the character is greater than 0
   AND:      ble $t3, $s5, THEN        # and less than 9 branch to LABEL1
-  THEN:			jal LABEL1
-            j JUMP
+  
             bge $t3, $s6, AND2        #if the character is greater than 'a'
   AND2:			ble $t3, $s7, THEN2       #and less than 'f' 
-  THEN2:		jal LABEL2                #jump to LABEL2
-  					j JUMP                    
+   
   					bge $t3, $t4, AND3        #if the character is greater than 'A'
   AND3:     ble $t3, $t5, THEN3       #and less than 'F' branch to LABEL3
-  THEN3: 		jal LABEL3
-  					j JUMP                    #skips else 
+  
             j ELSE                    #jump to ELSE
+            
+  THEN:			jal LABEL1                #jump to LABEL1
+            j JUMP                    #jump to bottom of loop
+            
+  THEN2:		jal LABEL2                #jump to LABEL2
+  					j JUMP                    #jump to bottom of loop
+  
+  THEN3: 		jal LABEL3                #jump to LABEL3
+  					j JUMP                    #jump to bottom of loop
+  
   JUMP:     addi $a1, $a1, 1          #increment the string pointer
             addi $t2, $t2, 1          #increment counter
             j FORLOOP                 #jump to the beginning of the loop
@@ -66,52 +73,64 @@ main:
   
   LABEL1:   
  						sub $t1, $t3, $s4          #get the int value of the character
- 						addu $t6, $s3, $t2         #(userInput.length()- counter)
+ 						sub $t6, $s3, $t2          #(userInput.length()- counter)
             li $t7, 1									 #loop counter 
  						li $t8, 16                 #base 16
  						li $v0, 16
+            
+            beq $t7, $zero, ZERO
  	LOOP:     beq $t7, $t6, DECIMAL      #if counter equals (userInput.length()- counter) end loop
  	          multu $v0, $t8             #16 *= 16
             mflo $v0 									 #store results in $v0 register
             addi $t7, $t7, 1           #increment counter
             j LOOP
  DECIMAL:   mult $t1, $v0
-            mflo $v1
+            j NONZERO
+ ZERO:      mult $t1, $t7
+ NONZERO:   mflo $v1
             addu $s1, $s1, $v1
             j EXIT
             
  
  LABEL2:   
- 						sub $t1, $t3, $s6          #get the int value of the character
+ 						sub $t1, $t3, $t4          #get the int value of the character
  						addi $t1, $t1, 10
- 						addu $t6, $s3, $t2         #(userInput.length()- counter)
+ 						sub $t6, $s3, $t2         #(userInput.length()- counter)
             li $t7, 1									 #loop counter 
  						li $t8, 16                 #base 16
  						li $v0, 16
+
+            beq $t7, $zero, ZERO2
  	LOOP2:    beq $t7, $t6, DECIMAL2     #if counter equals (userInput.length()- counter) end loop
  	          multu $v0, $t8             #16 *= 16
             mflo $v0 									 #store results in $v0 register
             addi $t7, $t7, 1           #increment counter
             j LOOP2
  DECIMAL2:  mult $t1, $v0
-            mflo $v1
+            j NONZERO2
+ ZERO2:     mult $t1, $t7
+ NONZERO2:  mflo $v1
             addu $s1, $s1, $v1
             j EXIT
             
   LABEL3:   
- 						sub $t1, $t3, $t4          #get the int value of the character
+ 						sub $t1, $t3, $s6          #get the int value of the character
  						addi $t1, $t1, 10
- 						addu $t6, $s3, $t2         #(userInput.length()- counter)
+ 						sub $t6, $s3, $t2         #(userInput.length()- counter)
             li $t7, 1									 #loop counter 
  						li $t8, 16                 #base 16
  						li $v0, 16
+  
+            beq $t7, $zero, ZERO3
  	LOOP3:    beq $t7, $t6, DECIMAL3     #if counter equals (userInput.length()- counter) end loop
  	          multu $v0, $t8             #16 *= 16
             mflo $v0 									 #store results in $v0 register
             addi $t7, $t7, 1           #increment counter
             j LOOP3
  DECIMAL3:  mult $t1, $v0
-            mflo $v1
+            j NONZERO3
+ ZERO3:     mult $t1, $t7
+ NONZERO3:  mflo $v1
             addu $s1, $s1, $v1
             j EXIT
  		
@@ -140,7 +159,8 @@ main:
           syscall
 
           la $a0, buffer               #reload byte space to primary address
-          addi $a0, $a1, 0             #primary address = t1 address (load pointer)
+          sub $a1, $a1, $s3 
+          addi $a0, $a1, 1             #primary address = t1 address (load pointer)
           li $v0, 4                    #system call code for printing string = 4
           syscall
         
